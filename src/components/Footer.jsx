9411f16ab87384cb70f2b091e98f49e7045d47e0
Suriya-17 <suriya791984@gmail.com>
2025-10-12 19:55:56 +0530

    Footer component added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+const FooterContainer = styled.div`
+        color: ${props => props.theme.colors.backgroundLight};
+        background-color:  ${props => props.theme.colors.backgroundDark};
+        font-family: ${props => props.theme.fonts.secondaryFont};
+        padding: 35px 300px 35px 300px;
+
+    
+`
+const FooterLinks = styled.div`
+        display: flex;
+        justify-content: space-between;
+        border-bottom: solid 1px ${props => props.theme.colors.primary};
+        padding: 20px;
+
+`
+
+const QuickLinks = styled.ul`
+    
+        display: flex;
+        gap: 20px;
+        list-style-type: none;
+        text-decoration: none;
+        li{
+            a{
+                text-decoration: none;
+                color: ${props => props.theme.colors.backgroundLight};
+                transition: color 0.3s ease-in-out;
+            }
+            a:hover{
+            color: ${props => props.theme.colors.primary};
+            }
+        }
+    
+`
+const SocialMediaLinks = styled.ul`
+        display: flex;
+        gap: 20px;
+        list-style-type: none;
+        li{
+            a{
+                text-decoration: none;
+                color: ${props => props.theme.colors.backgroundLight};
+                transition: color 0.3s ease-in-out;
+            }
+            a:hover{
+            color: ${props => props.theme.colors.primary};
+            }
+        }
+       
+`
+
+const CopyRightText = styled.p`
+        font-size: small;
+        text-align: center;
+        margin: 30px;
+`
+
+function Footer() {
+  return (
+    <div>
+        <FooterContainer>
+            <FooterLinks>
+                <QuickLinks>
+                    <li>
+                        <Link>Services</Link>
+                    </li>
+                    <li>
+                        <Link>Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link>About</Link>
+                    </li>
+                    <li>
+                        <Link>Contact</Link>
+                    </li>
+                </QuickLinks>
+                <SocialMediaLinks>
+                    <li>
+                        <Link><i className="fa-brands fa-facebook fa-xl"></i></Link>
+                    </li>
+                    <li>
+                        <Link><i className="fa-brands fa-instagram fa-xl"></i></Link>
+                    </li>
+                    <li>
+                        <Link><i className="fa-brands fa-twitter fa-xl"></i></Link>
+                    </li>
+                    <li>
+                        <Link><i className="fa-brands fa-linkedin fa-xl"></i></Link>
+                    </li>
+                    <li>
+                        <Link><i className="fa-brands fa-youtube fa-xl"></i></Link>
+                    </li>
+                </SocialMediaLinks>
+            </FooterLinks>
+            <CopyRightText> &copy; 2025 Arjun Digital Solutions. All Rights Reserved.</CopyRightText>
+        </FooterContainer>
+
+    </div>
+  )
+}
+
+export default Footer;
--- src/pages/development/Testingpage.jsx
+++ src/pages/development/Testingpage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import TestimonialCard from '../../components/TestimonialCard';
 import ServiceCard from '../../components/ServiceCard';
+import Footer from '../../components/Footer';
 
 function Testingpage() {
     return (
@@ -10,6 +11,7 @@ function Testingpage() {
             <Navbar />
             <TestimonialCard image="src\components\placeholder.jpg" quotes="SuperMan must be stopped!" clientName="Lex Luthor" clientDetails="CEO, LexCorp"/>
             <ServiceCard serviceName="Search Engine Optimization (SEO)" description = "Boost your online visiblity and attract more customers with our proven SEO strategies that are designed to rank you higher upon goggle searches" image="src/assets/placeholder.png"/>
+            <Footer/>
         </>
 
     )
